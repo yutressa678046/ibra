019ca495733038a6a0e90c401794fe8565766e88
ИбраимовА <@227_3>
2017-12-06 09:41:18 +0600

    Add project files.

---
+++ ConsoleApplication10/Source.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+int main()
+
+{
+	setlocale(LC_ALL, "Rus");
+
+    myStart:
+	int z = 0;
+	cout <<"Введите номер задания";
+	cin >> z;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
